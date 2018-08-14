ab8c6b442304fb432f847d955cddd9b1628a3623
tomyahu <tomyahu@gmail.com>
2018-08-14 18:59:40 -0300

    Hacer que los espacios de la grilla sean seleccionables

---
+++ cc4401Inventory/mainApp/static/mainApp/js/schedule_form.js
@@ -0,0 +1,6 @@
+$(".events-group").hover(function(){
+        $(this).css("background-color", "rgba(0,0,0,0.1)");
+        $(this).css("cursor", "pointer");
+    }, function(){
+        $(this).css("background-color", "rgba(0,0,0,0.0)");
+});
