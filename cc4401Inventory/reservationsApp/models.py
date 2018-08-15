a4865990bc561e4960f3372aa04c3a4d171aa0e0
tomyahu <tomyahu@gmail.com>
2018-08-15 15:02:10 -0300

    funcion __str__ agregada

--- cc4401Inventory/reservationsApp/models.py
+++ cc4401Inventory/reservationsApp/models.py
@@ -7,5 +7,11 @@ from django.db import models
 class Space_Reservation(Action):
     space = models.ForeignKey(Space, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.space.__str__() + ' - ' + self.user.__str__() + ' - (' + str(self.starting_date_time) + ')'
+
 class Article_Reservation(Action):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.article.__str__() + ' - ' + self.user.__str__() + ' - (' + str(self.starting_date_time) + ')'
