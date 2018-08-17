30b9df1e283a01334740a4a7f3a205aedcdc94fb
Adriana <adriana.concha@ing.uchile.cl>
2018-08-17 15:00:54 -0300

    Gestion de espacios .../space/id/edit

--- cc4401Inventory/spacesApp/urls.py
+++ cc4401Inventory/spacesApp/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('<int:space_id>', views.space_data, name='space_data'),
-
+    path('<int:space_id>/edit', views.space_data_admin, name='space_data_admin'),
+    path('<int:space_id>/edit_name', views.space_edit_name, name='space_edit_name'),
+    path('<int:space_id>/edit_image', views.space_edit_image, name='space_edit_image'),
+    path('<int:space_id>/edit_capacity', views.space_edit_capacity, name='space_edit_capacity'),
+    path('<int:space_id>/edit_description', views.space_edit_description, name='space_edit_description'),
 ]
--- cc4401Inventory/spacesApp/views.py
+++ cc4401Inventory/spacesApp/views.py
@@ -27,5 +27,66 @@ def space_data(request, space_id):
         print(e)
         return redirect('/')
 
+@login_required
+def space_data_admin(request, space_id):
+    if not request.user.is_staff:
+        return redirect('/')
+    else:
+        try:
+            space = Space.objects.get(id=space_id)
+            context = {
+                'space': space
+            }
+            return render(request, 'space_data_admin.html', context)
+        except:
+            return redirect('/')
+
+
+
+@login_required
+def space_edit_name(request, space_id):
+
+    if request.method == "POST":
+        a = Space.objects.get(id=space_id)
+        a.name = request.POST["name"]
+        a.save()
+    return redirect('/space/'+str(space_id)+'/edit')
+
+
+@login_required
+def space_edit_image(request, space_id):
+
+    if request.method == "POST":
+        u_file = request.FILES["image"]
+        extension = os.path.splitext(u_file.name)[1]
+        a = Space.objects.get(id=space_id)
+        a.image.save(str(space_id)+"_image"+extension, u_file)
+        a.save()
+
+    return redirect('/space/' + str(space_id) + '/edit')
+
+
+
+@login_required
+def space_edit_capacity(request, space_id):
+
+    if request.method == "POST":
+        c=request.POST["capacity"]
+        if c!="":
+            a = Space.objects.get(id=space_id)
+            a.capacity = c
+            a.save()
+    return redirect('/space/'+str(space_id)+'/edit')
+
+
+
+@login_required
+def space_edit_description(request, space_id):
+    if request.method == "POST":
+        a = Space.objects.get(id=space_id)
+        a.description = request.POST["description"]
+        a.save()
+
+    return redirect('/space/' + str(space_id) + '/edit')
 
 
