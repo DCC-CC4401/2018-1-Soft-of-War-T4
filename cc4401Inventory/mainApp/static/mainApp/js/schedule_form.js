481cde18ff88b53d334b677e08a0be0cd6a9deb7
tomyahu <tomyahu@gmail.com>
2018-08-17 18:17:05 -0300

    Bug que no se mostraba el día en el form en la grilla arreglado

--- cc4401Inventory/mainApp/static/mainApp/js/schedule_form.js
+++ cc4401Inventory/mainApp/static/mainApp/js/schedule_form.js
@@ -5,13 +5,15 @@ $(".events-group").hover(function () {
     $(this).css("background-color", "rgba(0,0,0,0.0)");
 });
 
-$('#reservaModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget); // Button that triggered the modal
-    var dia = button.data('day'); // Extract info from data-* attributes
-    var dias_array = ['Lunes','Martes','Miercoles','Jueves','Viernes'];
+$(window).bind("load", function() {
+   $('#reservaModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var dia = button.data('day'); // Extract info from data-* attributes
+        var dias_array = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'];
 
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $(this);
-    modal.find('.modal-title').text('Reserva un espacio el ' + dias_array[dia]);
-    modal.find('#dia_seleccionado').val(dia);
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this);
+        modal.find('.modal-title').text('Reserva un espacio el ' + dias_array[dia]);
+        modal.find('#dia_seleccionado').val(dia);
+    });
 });
