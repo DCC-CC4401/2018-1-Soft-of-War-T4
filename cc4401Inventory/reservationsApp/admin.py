8a39522e02ceff1924513174ec9b6146b06b4517
tomyahu <tomyahu@gmail.com>
2018-08-17 17:37:21 -0300

    ArticleReservation agregado

--- cc4401Inventory/reservationsApp/admin.py
+++ cc4401Inventory/reservationsApp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Space_Reservation
+from .models import Space_Reservation, Article_Reservation
 
 # Register your models here.
+admin.site.register(Article_Reservation)
 admin.site.register(Space_Reservation)
