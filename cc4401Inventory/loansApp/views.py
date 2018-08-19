331b3834ec44d2dca5b53980d6182b57639e14e6
Vicente Illanes <vicente.illanes.v@gmail.com>
2018-08-18 21:56:58 -0300

    login admin: landing page de admin

--- cc4401Inventory/loansApp/views.py
+++ cc4401Inventory/loansApp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from loansApp.models import Article_Loan
+from datetime import datetime
 
-# Create your views here.
+@login_required
+def article_data(request, loan_id):
+    try:
+        loan = Article_Loan.objects.get(id=loan_id)
+        context = {
+            'loan': loan,
+        }
+
+        return render(request, 'loans_data.html', context)
+    except Exception as e:
+        print(e)
+        return redirect('/')
+
