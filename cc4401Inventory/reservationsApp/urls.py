01ad8d9917d9cedf7668672ab3f0cc125968beb0
​ Asteriones​ < josefa.robert@ug.uchile.cl>
2018-08-19 03:44:37 -0300

    creado template para  detalle de una reserva

--- cc4401Inventory/reservationsApp/urls.py
+++ cc4401Inventory/reservationsApp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('delete/', views.delete, name='delete_reservation'),
-    path('<int:reservation_id>', views.article_reservation_data, name='view_reservation'),
+    path('<int:reservation_id>', views.view_reservation, name='view_reservation'),
 ]
--- cc4401Inventory/reservationsApp/views.py
+++ cc4401Inventory/reservationsApp/views.py
@@ -8,7 +8,7 @@ from loansApp.models import Article_Loan
 from django.db import models
 from datetime import datetime, timedelta
 from reservationsApp.models import Article_Reservation
-
+from mainApp.models import User
 import random, os
 import pytz
 from django.contrib import messages
@@ -29,34 +29,17 @@ def delete(request):
 
 
 @login_required
-def article_reservation_data(request, reservation_id):
-    try:
-        article = Article_Reservation.objects.get(id=reservation_id)
+def view_reservation(request, reservation_id):
 
-        last_loans = Article_Loan.objects.filter(article=article,
-                                         ending_date_time__lt=datetime.now(tz=pytz.utc)
-                                         ).order_by('-ending_date_time')[:10]
+    reservation = Article_Reservation.objects.get(id=reservation_id)
+    user =  reservation.user
+    article= reservation.article
 
-        loan_list = list()
-        for loan in last_loans:
+    context = {
+        'reservation': reservation,
+        'user': user,
+        'article': article,
+        'id' : reservation_id
+    }
 
-            starting_day = loan.starting_date_time.strftime("%d-%m-%Y")
-            ending_day = loan.ending_date_time.strftime("%d-%m-%Y")
-            starting_hour = loan.starting_date_time.strftime("%H:%M")
-            ending_hour = loan.ending_date_time.strftime("%H:%M")
-
-            if starting_day == ending_day:
-                loan_list.append(starting_day+" "+starting_hour+" a "+ending_hour)
-            else:
-                loan_list.append(starting_day + ", " + starting_hour + " a " +ending_day + ", " +ending_hour)
-
-
-        context = {
-            'article': article,
-            'last_loans': loan_list
-        }
-
-        return render(request, 'article_data.html', context)
-    except Exception as e:
-        print(e)
-        return redirect('/')
+    return render(request, 'reservation_data.html', context)
