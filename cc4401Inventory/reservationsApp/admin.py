30a3b90ea08c9545e2ac2466bc8c695fbc79994e
​ Asteriones​ < josefa.robert@ug.uchile.cl>
2018-08-19 01:49:38 -0300

    cambio en perfil usuario

--- cc4401Inventory/reservationsApp/admin.py
+++ cc4401Inventory/reservationsApp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Space_Reservation
+from .models import Space_Reservation, Article_Reservation
+
 
 # Register your models here.
 admin.site.register(Space_Reservation)
+admin.site.register(Article_Reservation)
--- cc4401Inventory/reservationsApp/views.py
+++ cc4401Inventory/reservationsApp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import Space_Reservation
 from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from articlesApp.models import Article
+from loansApp.models import Article_Loan
+from django.db import models
+from datetime import datetime, timedelta
+from reservationsApp.models import Article_Reservation
+
+import random, os
+import pytz
+from django.contrib import messages
 
 
 def delete(request):
@@ -15,3 +26,37 @@ def delete(request):
             messages.warning(request, 'Ha ocurrido un error y la reserva no se ha eliminado')
 
         return redirect('user_data', user_id=request.user.id)
+
+
+@login_required
+def article_reservation_data(request, reservation_id):
+    try:
+        article = Article_Reservation.objects.get(id=reservation_id)
+
+        last_loans = Article_Loan.objects.filter(article=article,
+                                         ending_date_time__lt=datetime.now(tz=pytz.utc)
+                                         ).order_by('-ending_date_time')[:10]
+
+        loan_list = list()
+        for loan in last_loans:
+
+            starting_day = loan.starting_date_time.strftime("%d-%m-%Y")
+            ending_day = loan.ending_date_time.strftime("%d-%m-%Y")
+            starting_hour = loan.starting_date_time.strftime("%H:%M")
+            ending_hour = loan.ending_date_time.strftime("%H:%M")
+
+            if starting_day == ending_day:
+                loan_list.append(starting_day+" "+starting_hour+" a "+ending_hour)
+            else:
+                loan_list.append(starting_day + ", " + starting_hour + " a " +ending_day + ", " +ending_hour)
+
+
+        context = {
+            'article': article,
+            'last_loans': loan_list
+        }
+
+        return render(request, 'article_data.html', context)
+    except Exception as e:
+        print(e)
+        return redirect('/')
