d8b87a456881adaadd7e0c1bbcaa582164d7e0dc
tomyahu <tomyahu@gmail.com>
2018-08-21 10:55:29 -0300

    Form espacios activado Bug de los años corregido

--- cc4401Inventory/spacesApp/views.py
+++ cc4401Inventory/spacesApp/views.py
@@ -18,15 +18,18 @@ def space_data(request, space_id, date=None):
         if date:
             current_date = date
             current_week = datetime.datetime.strptime(current_date, "%Y-%m-%d").date().isocalendar()[1]
+            current_year = datetime.datetime.strptime(current_date, "%Y-%m-%d").date().isocalendar()[0]
         else:
             try:
                 current_week = datetime.datetime.strptime(request.GET["date"], "%Y-%m-%d").date().isocalendar()[1]
+                current_year = datetime.datetime.strptime(request.GET["date"], "%Y-%m-%d").date().isocalendar()[0]
                 current_date = request.GET["date"]
             except:
                 current_week = datetime.date.today().isocalendar()[1]
+                current_year = datetime.date.today().isocalendar()[0]
                 current_date = datetime.date.today().strftime("%Y-%m-%d")
 
-        reservations = Space_Reservation.objects.filter(starting_date_time__week=current_week, state__in=['P', 'A'], id=space_id)
+        reservations = Space_Reservation.objects.filter(starting_date_time__week=current_week, starting_date_time__year=current_year, state__in=['P', 'A'], id=space_id)
         colores = {'A': 'rgba(0,153,0,0.7)', 'P': 'rgba(51,51,204,0.7)'}
 
         res_list = []
@@ -54,12 +57,69 @@ def space_data(request, space_id, date=None):
         monday = ((datetime.datetime.strptime(current_date, "%Y-%m-%d") - datetime.timedelta(days=delta)).strftime(
             "%d/%m/%Y"))
 
+        if request.method == 'POST':
+            try:
+                dia_reserva = int(request.POST['day'])
+                error = False
+
+                if dia_reserva < 0 or dia_reserva > 4:
+                    raise ValueError
+
+                espacio_str = request.POST['espacio_reservado']
+                hora_ini_str = request.POST['reserv_ini'].split(":")
+                hora_fin_str = request.POST['reserv_fin'].split(":")
+
+                hora_ini_num = int(hora_ini_str[0])*60 + int(hora_ini_str[1])
+                hora_fin_num = int(hora_fin_str[0])*60 + int(hora_fin_str[1])
+
+                if hora_ini_num >= hora_fin_num:
+                    messages.warning(request, 'La hora inicial debe ser después que la hora final.')
+                    error = True
+
+                splited_monday = monday.split("/")
+                fecha_ini_str = str(int(splited_monday[0]) + dia_reserva) + "-" + splited_monday[1] + "-" + splited_monday[2] + " " + ":".join(hora_ini_str)
+                fecha_fin_str = str(int(splited_monday[0]) + dia_reserva) + "-" + splited_monday[1] + "-" + splited_monday[2] + " " + ":".join(hora_fin_str)
+
+                fecha_ini = datetime.datetime.strptime(fecha_ini_str, '%d-%m-%Y %H:%M')
+                fecha_fin = datetime.datetime.strptime(fecha_fin_str, '%d-%m-%Y %H:%M')
+
+                if fecha_ini < datetime.datetime.now() + datetime.timedelta(hours=24) and espacio_str == "Quincho":
+                    messages.warning(request, 'Reserva rechazada, recuerda que los prestamos del Quincho caducan 24 horas antes de la fecha de inicio del préstamo.')
+                    error = True
+                elif fecha_ini < datetime.datetime.now() + datetime.timedelta(hours=1):
+                    messages.warning(request, 'No se pueden hacer reservas en un horario menor a una hora a partir de la hora actual.')
+                    error = True
+
+                if not request.user.enabled:
+                    messages.warning(request, "Debes estar habilitado para hacer reservas para hacer una reserva :/")
+                    error = True
+
+                espacio = Space.objects.filter(name=espacio_str).get()
+
+                reservas_del_dia_antes = Space_Reservation.objects.filter(starting_date_time__range=(fecha_ini, fecha_fin), state='A')
+                reservas_del_dia_despues = Space_Reservation.objects.filter(ending_date_time__range=(fecha_ini, fecha_fin), state='A')
+
+                if reservas_del_dia_antes or reservas_del_dia_despues:
+                    messages.warning(request, "Este espacio está ocupado en este itervalo de tiempo")
+                    error = True
+
+                if not error:
+                    reserva = Space_Reservation(space=espacio, starting_date_time=fecha_ini, ending_date_time=fecha_fin,
+                                        user=request.user)
+                    reserva.save()
+
+            except:
+                messages.warning(request, 'Formulario Incorrecto')
+
+        all_spaces = Space.objects.all()
         context = {
             'space': space,
             'reservations': res_list,
             'current_date': current_date,
             'controls': move_controls,
-            'actual_monday': monday
+            'actual_monday': monday,
+            'spaces': all_spaces,
+            'enable_filters': False
         }
 
         return render(request, 'space_data.html', context)
